192ad2a4c9bfea506dc594d96fd97b4c53cf64d6
cristianpb <felipebrokate@gmail.com>
2018-01-07 01:45:39 +0100

    update requirements

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# cdiscount documentation build configuration file, created by
+# PriceMinister documentation build configuration file, created by
 # sphinx-quickstart.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'cdiscount'
+project = u'PriceMinister'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 # html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'cdiscountdoc'
+htmlhelp_basename = 'PriceMinister'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -184,8 +184,8 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
     ('index',
-     'cdiscount.tex',
-     u'cdiscount Documentation',
+     'PriceMinister.tex',
+     u'PriceMinister Documentation',
      u"cristianpb", 'manual'),
 ]
 
@@ -215,7 +215,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'cdiscount', u'cdiscount Documentation',
+    ('index', 'PriceMinister', u'PriceMinister Documentation',
      [u"cristianpb"], 1)
 ]
 
@@ -229,8 +229,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'cdiscount', u'cdiscount Documentation',
-     u"cristianpb", 'cdiscount',
+    ('index', 'PriceMinister', u'PriceMinister Documentation',
+     u"cristianpb", 'PriceMinister',
      'L’objectif de ce challenge est de prédire si l’avis d’un utilisateur sur un produit sera utile', 'Miscellaneous'),
 ]
 
